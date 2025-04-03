e36f785a6a4f575ddd6c76ad6dab263bec47e174
ajjufam <prajwal.au768@gmail.com>
2025-04-03 23:44:36 +0530

    changes in arrays

--- Arrays/1_largest_n_smallest_element.js
+++ Arrays/1_largest_n_smallest_element.js
@@ -2,6 +2,11 @@ function largestElement(arr) {
   return Math.max(...arr);
 }
 
+function smallestElement(arr) {
+  return Math.min(...arr);
+}
+
 const arr = [20, 30, 40, 10, 98];
 console.log("Spread operators", ...arr); // 20 30 40 10 98
 console.log(`Largest element in the given array is`, largestElement(arr)); // 98
+console.log(`Smallest element in the given array is`, smallestElement(arr)); // 10
--- Arrays/2_nth_largest_n_smallest_element.js
+++ Arrays/2_nth_largest_n_smallest_element.js
@@ -3,7 +3,13 @@ function nthLargestNumber(arr, n) {
   return set.length > 1 ? set[n - 1] : null; //ternary opearator
 }
 
+function nthSmallestNumber(arr, n) {
+  const set = [...new Set(arr)].sort((a, b) => a - b);
+  return set.length > 1 ? set[n - 1] : null;
+}
+
 const arr = [8, 9, 1, 2, 3, 2, 4, 5, 4, 6, 7, 8];
 const n = 2;
 
 console.log(`${n}th Largest Number is :`, nthLargestNumber(arr, n));
+console.log(`${n}th Smallest Number is :`, nthSmallestNumber(arr, n));
